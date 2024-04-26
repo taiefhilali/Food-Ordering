3898d12f565556e183a94c8b58ac945e34130227
Taief HILALI <62665645+taiefhilali@users.noreply.github.com>
2024-04-26 23:40:25 +0100

    usertype fixed!

--- backend/src/middleware/verifyToken.ts
+++ backend/src/middleware/verifyToken.ts
@@ -1,46 +1,48 @@
+import { body } from 'express-validator';
 import { NextFunction, Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
 
-
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
-
+   console.log(authHeader);
     if (authHeader) {
         const token = authHeader.split(' ')[1];
 
         jwt.verify(token, process.env.JWT_SECRET as string, (err: jwt.VerifyErrors | null, decoded: any) => {
             if (err) {
+                console.error('Error decoding token:', err);
                 return res.status(403).json({ message: 'Invalid token' });
             }
+
+            console.log('Decoded token:', decoded);
 
             // Attach the decoded user object to the request object
             (req as any).user = decoded;
             next();
         });
     } else {
-        // If no authorization header is present
+        console.error('No authorization header present');
         return res.status(401).json({ message: 'Unauthorized' });
     }
 };
-
 const verifyUserType = (
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
+    // Access decoded user object from the request
+    const decodedUser = (req as any).user;
 
-    verifyToken(req, res, () => {
+    if (decodedUser && (decodedUser.userType === "Client" || decodedUser.userType === "Admin" || decodedUser.userType === "Vendor")) {
+        next();
+    } else {
+        res.status(403).json({
+            message: 'Invalid user type'
+        });
+    }
+};
 
-        if ((req as any).user.UserType === "Client" || (req as any).user.UserType === "Admin" || (req as any).user.UserType === "Vendor") {
-            next();
 
-        } else (
-            res.status(403).json({
-                message: 'Invalid user type'
-            })
-        )
-    });
-}
 
 const verifyVendor = (
     req: Request,
