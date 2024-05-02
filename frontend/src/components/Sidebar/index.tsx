9b1cac2425fae2cf3feecf8a228fa8b72dc8ffda
Taief HILALI <62665645+taiefhilali@users.noreply.github.com>
2024-05-03 00:00:14 +0100

    changes

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -357,6 +357,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                             : setSidebarExpanded(true);
                         }}
                       >
+
+
                         <svg
                           className="fill-current"
                           width="18"
@@ -392,7 +394,11 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                           fill="none"
                           xmlns="http://www.w3.org/2000/svg"
                         >
-                          {/* Rest of the SVG paths */}
+                          {/* Sample path for the dropdown icon */}
+                          <path
+                            d="M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z"
+                            fill="#000000"
+                          />
                         </svg>
                       </NavLink>
                       {/* Dropdown Menu */}
@@ -427,10 +433,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                           <li>
                             <NavLink
                               to="/display-products"
-                              className={({ isActive }) =>
-                                'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ' +
-                                (isActive && '!text-white')
-                              }
+                              className="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
+                              activeClassName="text-white"
                             >
                               <svg
                                 className="fill-current"
@@ -459,6 +463,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                               </svg>
                               Product Details
                             </NavLink>
+
                           </li>
                         </ul>
                       </div>
@@ -483,11 +488,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
 
               <li>
 
-                <ul>
-                  <li>
 
-                  </li>
-                </ul>
               </li>
 
 
