2cbf1180c9e973061ae3e6f99af72fe99e40b274
Taief HILALI <62665645+taiefhilali@users.noreply.github.com>
2024-06-26 16:17:47 +0100

    logo adjust

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -65,7 +65,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center  justify-between  px-6 py-5.5 lg:py-6.5">
         <NavLink to="/">
-          <img src={Logo} alt="Logo" />
+          <img src={Logo} style={{ marginLeft: '-0.5cm' }} alt="Logo" />
         </NavLink>
 
         <button
