317e61ea9edc1bac6ad5a9ebf8ab55c75cbaab67
Taief HILALI <62665645+taiefhilali@users.noreply.github.com>
2024-09-04 23:50:52 +0000

    add compose.yml

--- backend/src/controllers/CartController.ts
+++ backend/src/controllers/CartController.ts
@@ -5,9 +5,13 @@ import stripe from 'stripe';
 import User from '../models/User';
 import { io } from './../index' 
 
+require('dotenv').config(); // Ensure you have dotenv configured to load environment variables
 
-const stripeSecretKey = 'sk_test_51PM7rN03qVjqSurgaFDcUo3Y1GrtFJoYzoiHZZRIWvNhaIec7DrXqNPLFuori2tTwAjBPEQwHF4UOuLBIptnxx4m00OwswBdhb'; // Replace with your actual Stripe secret key
-const stripeClient = new stripe(stripeSecretKey);
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripeClient = require('stripe')(stripeSecretKey);
+
+// const stripeSecretKey = 'sk_test_51PM7rN03qVjqSurgaFDcUo3Y1GrtFJoYzoiHZZRIWvNhaIec7DrXqNPLFuori2tTwAjBPEQwHF4UOuLBIptnxx4m00OwswBdhb'; // Replace with your actual Stripe secret key
+// const stripeClient = new stripe(stripeSecretKey);
 interface CartItem {
   itemType: string;
   item: mongoose.Schema.Types.ObjectId;
