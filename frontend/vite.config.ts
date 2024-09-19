e59be76ae318a047b6fa107585123733b72146d8
Taief HILALI <62665645+taiefhilali@users.noreply.github.com>
2024-09-19 11:24:01 +0000

    push build added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path"
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -8,10 +8,12 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
       'jsvectormap': '/node_modules/jsvectormap'
-
     },
   },
-  server:{
-    port:3000
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'build', // Ensure this matches the directory you expect
   }
-})
+});
